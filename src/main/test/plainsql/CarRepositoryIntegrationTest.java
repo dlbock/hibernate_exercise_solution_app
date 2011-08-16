4442b2f540559135b953523b4d3e706ca65f35b6
dahlia <dlbock@gmail.com>
2011-08-16 16:01:20 +0530

    Cleaning up CarRepositoryTest

--- src/main/test/plainsql/CarRepositoryIntegrationTest.java
+++ src/main/test/plainsql/CarRepositoryIntegrationTest.java
@@ -12,17 +12,17 @@ public class CarRepositoryIntegrationTest {
     private CarRepository repository;
 
     @Before
-    public void clearDB() throws Exception {
+    public void cleanRepository() throws Exception {
         repository = new CarRepository();
         repository.update("DELETE FROM cars");
     }
 
     @Test
     public void shouldSaveCarsToRepository() throws SQLException {
-        insertToRepository(new Car("Ford", "JFK256"));
-        insertToRepository(new Car("Toyota", "ABC999"));
-        insertToRepository(new Car("Honda", "XYZ789"));
-        insertToRepository(new Car("GM", "GHJ123"));
+        insertToRepository(createCar("Ford", "JFK256"));
+        insertToRepository(createCar("Toyota", "ABC999"));
+        insertToRepository(createCar("Honda", "XYZ789"));
+        insertToRepository(createCar("GM", "GHJ123"));
 
         assertEquals(4, repository.findAll("SELECT * FROM cars"));
     }
@@ -30,5 +30,9 @@ public class CarRepositoryIntegrationTest {
     private void insertToRepository(Car car) throws SQLException {
         String sqlStatement = "INSERT INTO cars(name,numberPlate) VALUES('%s', '%s')";
         repository.update(String.format(sqlStatement, car.getName(), car.getNumberPlate()));
+    }
+
+    private Car createCar(String name, String numberPlate) {
+        return new Car(name, numberPlate);
     }
 }
